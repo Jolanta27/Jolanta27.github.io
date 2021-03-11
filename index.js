1ecf7116ad40c04af03602d1f4bd7b9b1eb993bf
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2021-03-11 10:18:27 +0100

    add scroll back to top button

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// change of size on logo img
 window.addEventListener('scroll', function() {
     const logoImage = document.getElementById("logo");
 
@@ -8,9 +9,29 @@ window.addEventListener('scroll', function() {
     }
 });
 
+// Add navbar button on mobile version 
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function() {
     mainNav.classList.toggle('active');
 });
+
+// Create a scroll back to top
+let myButton = document.getElementById('myBtn');
+
+window.onscroll = function() {
+    scrollFunction();
+}
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; //For Safari
+    document.documentElement.scrollTop = 0; //For Chrome, Firefox, IE and Opera
+}
